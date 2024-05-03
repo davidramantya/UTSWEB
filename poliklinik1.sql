8f0fa495a536d7c9e570e47230d960ae1d5ffcc1
davidramantya <71579840+davidramantya@users.noreply.github.com>
2024-05-03 10:25:33 +0700

    Add files via upload

---
+++ poliklinik1.sql
@@ -0,0 +1,253 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 02, 2024 at 11:03 AM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poliklinik1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_periksa`
+--
+
+CREATE TABLE `detail_periksa` (
+  `id` int(11) NOT NULL,
+  `id_periksa` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id`, `nama`, `alamat`, `no_hp`) VALUES
+(51, 'Dr. Fata', 'Semarang', '09983575'),
+(52, 'Dr. Raihan', 'Semarang', '02588468'),
+(53, 'Wahyu', 'Semarang', '01896434647');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id` int(11) NOT NULL,
+  `nama_obat` varchar(255) DEFAULT NULL,
+  `kemasan` varchar(100) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`) VALUES
+(9, 'Alopurinol tablet 100 mg', 'ktk 10 x 10 tablet ', 16000),
+(10, 'Alprazolam tablet 0,5 mg', 'ktk 10 x 10 tablet', 77000),
+(12, 'Ambroxol sirup 30 mg', 'ktk 10 x 10 tablet', 21000),
+(13, 'Clindamycin', 'ktk 10 x 10 tablet', 20500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `nama`, `alamat`, `no_hp`) VALUES
+(17, 'Binnus', 'Pati', '0821588635'),
+(18, 'Nova', 'Kebumen', '015256524'),
+(20, 'Vender', 'Kudus', '0895314524');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periksa`
+--
+
+CREATE TABLE `periksa` (
+  `id` int(11) NOT NULL,
+  `id_pasien` int(11) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `tgl_periksa` datetime NOT NULL,
+  `catatan` text NOT NULL,
+  `id_obat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `id_pasien`, `id_dokter`, `tgl_periksa`, `catatan`, `id_obat`) VALUES
+(57, 17, 51, '2024-05-02 17:47:00', 'jerawat', '13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('dokter','pasien') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
+(9, 'Binnus', '$2y$10$W/2VmpuHPG7ZdcOx4ojrg.Bo2WHZgjj/lw.ajlLzKWtJNSMUpBIQ6', 'dokter'),
+(10, 'Nova', '$2y$10$yLaySkVLNQmJhBjY9SpT..JTSuaSuHJj6V6F7HdkFx1kOHbDFdRni', 'dokter'),
+(11, 'Rian', '$2y$10$bWdfamsbAY2eq9m1LBrqoubk4X1BPWNQyjcOS/4hez.goTKo3xUJi', 'dokter'),
+(12, 'Bintang', '$2y$10$R5UZRPfVDSthPpjob7637OaDU1pYsdHNvlX5/1ru0bE7QwPPnZRLG', 'dokter');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_detail_periksa_periksa` (`id_periksa`),
+  ADD KEY `fk_detail_periksa_obat` (`id_obat`);
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pasien` (`id_pasien`,`id_dokter`),
+  ADD KEY `id_dokter` (`id_dokter`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `periksa`
+--
+ALTER TABLE `periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD CONSTRAINT `fk_detail_periksa_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_detail_periksa_periksa` FOREIGN KEY (`id_periksa`) REFERENCES `periksa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD CONSTRAINT `periksa_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `periksa_ibfk_2` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
